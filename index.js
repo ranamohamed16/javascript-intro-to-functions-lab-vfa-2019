fd29aae72b77373aca6f5be701dca6fcc9e1eab1
Rana  Mohamed <ranamohamed@alumni.psu.edu>
2019-06-23 15:45:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log() string.toUpperCase
+  console.log(string.toUpperCase())
 } 
 function logWhisper(string) {
-  console.log() string.toLowerCase
+  console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase( )
+  if (string.toLowerCase()===string) {
+    return "I can't hear you!";
+}
+else if ((string)==lowercase) {
+  return "I can't hear you!"
+}
+else if ((string)=="I love you, Grandma.")) {
+  return "I love you, too."
 }
